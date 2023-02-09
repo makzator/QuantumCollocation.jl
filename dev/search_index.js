afdb59f5ef7fe6d1785ea571265b406a4aad3795
Documenter.jl <documenter@juliadocs.github.io>
2023-02-09 21:16:42 +0000

    build based on 778afe4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pico","category":"page"},{"location":"#Pico","page":"Home","title":"Pico","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Pico.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pico]","category":"page"}]
+}
